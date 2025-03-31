d5e515fb85de2b77afb1078780d2aeceaa121f86
cOdES2424 <Mike.Bolz@OEServices.net>
2025-03-31 17:48:19 -0500

    Update ViolationSearch.py

--- ViolationSearch.py
+++ ViolationSearch.py
@@ -41,11 +41,15 @@ print('Logged in successfully')
 # Step 4: Function to navigate pages and scrape data
 def scrape_data(page_number):
     date_14_days_ago = (datetime.now() - timedelta(days=14)).strftime('%m/%d/%Y')
-    url = (f'https://apps.occ.ok.gov/PSTPortal/PublicImaging/Home#SearchByDate'
-           f'&DateRangeFrom={date_14_days_ago}&DateRangeTo={date_14_days_ago}'
-           f'&btnSubmitDateSearch=Search+by+Date+Range&pageNumber={page_number}')
+    search_data = {
+        'DateRangeFrom': date_14_days_ago,
+        'DateRangeTo': date_14_days_ago,
+        'btnSubmitDateSearch': 'Search by Date Range',
+        'pageNumber': page_number
+    }
+    url = 'https://apps.occ.ok.gov/PSTPortal/PublicImaging/Home'
     
-    response = session.get(url)
+    response = session.post(url, data=search_data)
 
     if response.status_code != 200:
         print(f'Failed to navigate to page {page_number}')
